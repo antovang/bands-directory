50ab83f4c6b7fcf2d9ef29edb3146d256b9e4725
Antonio Van Grieken <antovang@gmail.com>
2022-02-11 23:26:24 +0100

    First Commit

---
+++ app.jsx
@@ -0,0 +1,127 @@
+'use strict';
+
+var logged_in = true;
+var musiciens = [];
+var groupes = [];
+
+var data = {
+    Groupes : [
+        {id:"1",nom:"Beatles",nbMusiciens:4},
+        {id:"2",nom:"Pink Floyd",nbMusiciens:4},
+        {id:"3",nom:"Imagine Dragons",nbMusiciens:4},
+        {id:"4",nom:"Coldplay",nbMusiciens:4}
+    ],
+    Musiciens : [
+        {id:"1",prenom:"Paul", nom:"McCartney",groupe:"Beatles"},
+        {id:"2",prenom:"John", nom:"Lennon",groupe:"Beatles"},
+        {id:"3",prenom:"Ringo", nom:"Star",groupe:"Beatles"},
+        {id:"4",prenom:"George", nom:"Harrison",groupe:"Beatles"},
+    ]
+}
+
+for (const item of Object.values(data.Musiciens)){
+    let dynamicProps = item;
+    musiciens.push(<LigneMusicien {...dynamicProps} />)
+}
+
+for (const item of Object.values(data.Groupes)){
+    let dynamicProps = item;
+    groupes.push(<LigneGroupe {...dynamicProps} />)
+}
+
+function LigneMusicien(props){
+
+    return (
+        <tr>
+            <th scope="row">{props.id}</th>
+            <td> {props.prenom} </td>
+            <td> {props.nom}</td>
+            <td> {props.groupe} </td>
+            {logged_in
+                ? <td><DeleteButton /></td>
+                : <td><LookButton /></td>
+            }
+        </tr>
+    );
+}
+
+function DeleteButton(){
+    return (
+        <button className="nav-link" className="btn btn-danger" href="#" > Delete </button>
+    );
+}
+
+function LookButton(){
+    return (
+        <button className="nav-link" className="btn btn-primary" href="#" > Voir </button>
+    );
+}
+
+function LigneGroupe(props){
+
+    return (
+        <tr>
+            <th scope="row">{props.id}</th>
+            <td> {props.nom}</td>
+            <td> {props.nbMusiciens} </td>
+            {logged_in
+                ? <td><DeleteButton /></td>
+                : <td><LookButton /></td>
+            }
+        </tr>
+    );
+}
+
+function LoginForm(){
+
+    return (
+        <form id="loginForm" method="post">
+            <div className="mb-3">
+                <label htmlFor="exampleInputLogin1" className="form-label">Login</label>
+                <input type="text" name="login" className="form-control" id="loginInput"/>
+                    <div id="emailHelp" className="form-text">We'll never share your login with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" name="password"  className="form-control" id="pwInput"/>
+            </div>
+            <button class="btn btn-success" onClick={loginCheck}>Submit</button>
+        </form>
+    );
+}
+
+function loginCheck(){
+    let login = $('#loginInput').val();
+    let pw = $('#pwInput').val();
+
+    $.ajax({
+        method: "POST",
+        url: "script.php",
+        data: { login: login, password: pw }
+    })
+        .done(function( msg ) {
+            if(msg == "Success") {
+                alert("OK");
+                logged_in = true;
+            }else{
+                alert("KO")
+                logged_in = false;
+            }
+    });
+}
+
+const loginContainer = document.querySelector('#loginFormContainer');
+const tableauMusiciensContainer = document.querySelector('#ligneMusicien');
+const tableauGroupesContainer = document.querySelector('#ligneGroupe');
+
+ReactDOM.render(
+    <LoginForm />
+    ,loginContainer);
+
+ReactDOM.render(
+    musiciens
+    ,tableauMusiciensContainer);
+
+ReactDOM.render(
+    groupes
+    ,tableauGroupesContainer);
---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Annuaire</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script defer src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+</head>
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Groupes</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Musiciens</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <?php if(!isset($_SESSION)){ ?>
+                            <button id="logButton" class="btn btn-success">Log in</button>
+                        <?php }else{ ?>
+                            <button id="logButton" class="btn btn-danger">Log out</button>
+                        <?php } ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <h1>Annuaire en ligne</h1>
+    <p>La mairie de Grenoble propose son annuaire 100% dématérialisé</p>
+
+
+</header>
+
+<section id="groupes" class="container">
+    <!-- We will put our React component inside this div. -->
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Groupe</th>
+            <th scope="col"># Musiciens</th>
+            <th scope="col"></th>
+        </tr>
+        </thead>
+        <tbody id="ligneGroupe"></tbody>
+    </table>
+</section>
+
+<section id="musiciens" class="container">
+    <!-- We will put our React component inside this div. -->
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Prenom</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Groupe</th>
+            <th scope="col"></th>
+        </tr>
+        </thead>
+        <tbody id="ligneMusicien"></tbody>
+    </table>
+</section>
+
+<section id="login" class="container">
+    <!-- We will put our React component inside this div. -->
+    <div id="loginFormContainer"></div>
+</section>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Load React. -->
+<!-- Note: when deploying, replace "development.js" with "production.min.js". -->
+<script defer src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+<script defer src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+
+<!-- Load our React component. -->
+<script src="app.jsx" type="text/babel"></script>
+
+</body>
+</html>
